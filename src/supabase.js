e2315b8405aaa8a6eb737e79c9e8f849e00e0ebe
usertype46-collab <usertype46@gmail.com>
2026-07-26 09:45:15 +0800

    Delete src directory

--- src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = "https://cnkxsxhgdtuxknrzufhv.supabase.co";
-const SUPABASE_KEY = "sb_publishable_QEoX_f9G_Gf9kaaDZpaH-g_ageY5WFK";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
